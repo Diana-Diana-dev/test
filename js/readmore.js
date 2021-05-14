c19141da83f22527975a0670a709de927e8b46b7
Diana-Diana-dev <77507565+Diana-Diana-dev@users.noreply.github.com>
2021-05-14 11:58:41 +0300

    Add files via upload

---
+++ js/readmore.js
@@ -0,0 +1,63 @@
+
+
+ let dots = document.getElementById("dots");
+ let more = document.getElementById("more");
+ let buttonText = document.getElementById("buttonText");      
+
+// .addEventListener('click', readMore);
+
+function readMore(){
+
+if(dots.style.display === "none") {
+	dots.style.display = "inline";
+	more.style.display = "none";
+	buttonText.innerHTML = "Читать полностью";
+}else{
+	dots.style.display = "none";
+	more.style.display = "inline";
+	buttonText.innerHTML = "Скрыть";
+}
+}
+
+
+
+
+ let dots2 = document.getElementById("dots2");
+ let more2 = document.getElementById("more2");
+ let buttonText2 = document.getElementById("buttonText2");
+
+
+
+function readMore2(){
+
+if(dots2.style.display === "none") {
+	dots2.style.display = "inline";
+	more2.style.display = "none";
+	buttonText2.innerHTML = "Читать полностью";
+}else{
+	dots2.style.display = "none";
+	more2.style.display = "inline";
+	buttonText2.innerHTML = "Скрыть";
+}
+}
+
+
+
+ let dots3 = document.getElementById("dots3");
+ let more3 = document.getElementById("more3");
+ let buttonText3 = document.getElementById("buttonText3");
+
+
+
+function readMore3(){
+
+if(dots3.style.display === "none") {
+	dots3.style.display = "inline";
+	more3.style.display = "none";
+	buttonText3.innerHTML = "Читать полностью";
+}else{
+	dots3.style.display = "none";
+	more3.style.display = "inline";
+	buttonText3.innerHTML = "Скрыть";
+}
+}
